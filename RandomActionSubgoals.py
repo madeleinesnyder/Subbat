c2fc32ae64122ccdccfa877b23224e9236457c08
Ryan Moughan <rmoughan@berkeley.edu>
2019-11-18 10:06:59 -0800

    some debugging code added for Madeleine

--- RandomActionSubgoals.py
+++ RandomActionSubgoals.py
@@ -5,6 +5,7 @@ import numpy as np
 from utils import isInAir, getJumpOutcome
 from Action_Replay_Buffer import ActionReplayBuffer
 import matplotlib.pyplot as plt
+import pdb
 
 
 env = gym.make('MontezumaRevengeNoFrameskip-v4')
@@ -17,15 +18,13 @@ env.seed(42)
 ARP = ActionReplayBuffer()
 Goals = []
 
-# from PIL import Image
-#
-# obs= env.reset()
-# obs[115:117,:,:] = np.ones((2, 1, 3))
-# img = Image.fromarray(obs, 'RGB')
-# img.show()
+# all_actions = [[np.random.randint(0,18) for _ in range(6000)] for _ in range(10)]
 
+# all_actions = [[4,4,4,1] + [0 for _ in range(5595)] for _ in range(10)]
+
+# env.render()
 num_random_actions = 6000
-for episode in range(10):
+for episode in range(1000):
     observation = env.reset()
     total_reward = 0
     LastInAir = False
@@ -38,13 +37,16 @@ for episode in range(10):
         if total_reward == 400:
             print("Congratulations!! You've reached the end of the first room :)")
             break
+        # env.render()
         inAir = isInAir(env, obs, action, last_action)
         jumping = LastInAir == False and inAir == True
         if jumping:
             jump_outcome = getJumpOutcome(env, original_lives)
+            # print(t, action, "jumping", jump_outcome)
             jumping = False
             ARP.store(obs, action, jump_outcome, env)
-        if not jumping and not inAir:
+        if (not jumping) and (not inAir):
+            # print(t, "storing from the ground")
             ARP.store(obs, action, reward, env)
         LastInAir = inAir
         last_action = action
@@ -53,12 +55,15 @@ for episode in range(10):
     subgoals = ARP.find_subgoals()
     print(len(subgoals))
 
+#the first issue is at timestep 22
+
 #plot a heatmap
 obs = env.reset()
 plt.imshow(obs)
 for subgoal_location in subgoals:
     plt.scatter(subgoal_location[0], subgoal_location[1], c = "yellow", alpha = 0.5, s = 10)
 plt.show()
-
+pdb.set_trace()
+print("Done")
 
 #timestep 905 doesn't realize jumping off teadmill and dying is -1 reward
