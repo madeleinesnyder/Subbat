ac928ad6f5518885c2b8a081aee25e5c35b03673
Calvin Chi <calvinchi@Calvins-MacBook-Pro-3.local>
2019-11-19 15:43:35 -0800

    replay_memory reads and writes

--- main_replicate.py
+++ main_replicate.py
@@ -13,13 +13,14 @@ from isInAir import *
 from utils import *
 from getJumpOutcome import *
 import pdb
+import sys
 
 '''
 Initialize the environment and h-params (adjust later)
 '''
 random.seed(42)
 learning_rate = 2.5e-4
-num_episodes = 5
+num_episodes = 20
 num_pre_training_episodes = 5
 discount = 0.99
 batch_size = 256
@@ -34,16 +35,17 @@ tf.global_variables_initializer().run(session=sess)
 '''
 Initialize the replay buffers
 '''
-d1 = ReplayMemory()
-d2 = ReplayMemory()
+d1 = ReplayMemory(name = "controller", write_capacity = 20)
+d2 = ReplayMemory(name = "metacontroller", write_capacity = 20)
 
 '''
 Initialize subgoals 
 '''
 goals_xy = np.load("subgoals.npy")
-goals = {}
-for i in range(len(goals_xy)):
-    goals[i] = goals_xy[i]
+#goals = {}
+#for i in range(len(goals_xy)):
+#    goals[i] = goals_xy[i]
+goals = {0: goals_xy[0]}
 goal_dim = len(goals)
 
 '''
@@ -72,18 +74,19 @@ for i in range(num_pre_training_episodes):
     next_lives = 6
     goal_idx = random_goal_idx(goal_dim)
     goal_xy = goals[goal_idx]
+    goal_mask = convertToBinaryMask([(goal_xy[0] - 5, goal_xy[1] - 5),(goal_xy[0] + 5, goal_xy[1] + 5)])
+
     while not (done or dead):
         F = 0
         initial_observation = observation
         iteration = 0
         while not (done or at_subgoal or dead):
             if iteration % 10 == 0:
-                print("iteration {0} of episode {1}; ALE lives {2}".format(iteration, i, lives))
-
-            goal_mask = convertToBinaryMask([(goal_xy[0] - 5, goal_xy[1] - 5),(goal_xy[0] + 5, goal_xy[1] + 5)])
+                print("iteration {0} of episode {1}; ALE lives {2}; controller epsilon {3}".format(iteration, i, lives, controller.epsilon))
 
             # Get an action from the controller.
-            action = controller.epsGreedy(np.concatenate([observation, goal_mask], axis = 0), env.action_space)
+            observation_goal = np.concatenate([observation, goal_mask], axis = 0)
+            action = controller.epsGreedy(observation_goal[np.newaxis, :, :, :], env.action_space)
 
             # STEP THE ENV
             next_observation, f, done, next_lives = env.step(action)
@@ -92,11 +95,11 @@ for i in range(num_pre_training_episodes):
             dead = next_lives['ale.lives'] < lives
 
             # Record the reward if reached the subgoal.
-            if iteration % 10 == 0:
-                print("ALE coord: {0}".format(get_ALE_coord(env, observation)))
-                print("goal coord: {0}".format(goal_xy))
+            #if iteration % 10 == 0:
+            #    print("ALE coord: {0}".format(get_ALE_coord(env, observation)))
+            #    print("goal coord: {0}".format(goal_xy))
 
-            at_subgoal = achieved_subgoal(env, next_observation, goal_xy)
+            #at_subgoal = achieved_subgoal(env, next_observation, goal_xy)
             if at_subgoal:
                 print("subgoal achieved at iteration {0} of episode {1}".format(iteration, i))
                 r = 1
@@ -108,6 +111,7 @@ for i in range(num_pre_training_episodes):
 
             # Sample a batch from the buffer if there's enough in the buffer
             controller_batch = d1.sample(batch_size)
+
             # Get the controller targets
             c_targets = controller_targets(controller_batch[2], controller_batch[3], controller, discount)
 
@@ -120,10 +124,12 @@ for i in range(num_pre_training_episodes):
             # Update the observation
             observation = next_observation
             iteration += 1
-        d2.store(initial_observation, goal_idx, F, next_observation)
+        #d2.store(initial_observation, goal_idx, F, next_observation)
         if not (done or dead):
             goal_idx = random_goal_idx(goal_dim)
             goal_xy = goals[goal_idx]
+            at_subgoal = False 
     controller.anneal()
+    
 env.close()
 
--- replay_memory.py
+++ replay_memory.py
@@ -1,31 +1,110 @@
 import numpy as np
+import h5py
+import os
+
 
 class ReplayMemory:
 
-    def __init__(self, capacity = np.float("inf")):
+    def __init__(self, name, write_capacity = 20, capacity = 1000):
         self.capacity = capacity
-        #self.memories = np.empty(shape = (0, 4))
+        self.write_capacity = write_capacity
+        self.name = name
         self.memories = []
+        # TEST
+        self.ret = []
+        if not os.path.exists("replay_buffer"):
+            os.makedirs("replay_buffer")
+
+        if os.path.exists("replay_buffer/" + self.name + ".hdf5"):
+            with h5py.File("replay_buffer/" + self.name + ".hdf5", 'r') as f:
+                self.memories = [f['obs_t_goal'][:], f['goal_or_action'][:], f['reward'][:], f['obs_tp1_goal'][:]]
+        else:
+            with h5py.File("replay_buffer/" + self.name + ".hdf5", "w") as f:
+                if self.name == "controller":
+                    self.obs_goal_shape = [0, 420, 160, 3]
+                elif self.name == "metacontroller":
+                    self.obs_goal_shape = [0, 210, 160, 3]
+                f.create_dataset("obs_t_goal", self.obs_goal_shape, maxshape = [None] + self.obs_goal_shape[1:])
+                f.create_dataset("goal_or_action", (0,), maxshape = [None,])
+                f.create_dataset("reward", (0,), maxshape = [None,])
+                f.create_dataset("obs_tp1_goal", self.obs_goal_shape, maxshape = [None] + self.obs_goal_shape[1:])
+
 
     def store(self, arg1, arg2, arg3, arg4):
         if len(self.memories) == 0:
-            self.memories = [arg1[np.newaxis, :, :, :], np.array(arg2), np.array(arg3), arg4[np.newaxis, :, :, :]]
-        self.memories[0] = np.concatenate((self.memories[0], arg1[np.newaxis, :, :, :]), axis = 0)
-        self.memories[1] = np.append(self.memories[1], arg2)
-        self.memories[2] = np.append(self.memories[2], arg2)
-        self.memories[3] = np.concatenate((self.memories[3], arg4[np.newaxis, :, :, :]), axis = 0)
+            self.memories = [arg1[np.newaxis, :, :, :], np.array([arg2]), np.array([arg3]), arg4[np.newaxis, :, :, :]]
+        else:
+            self.memories[0] = np.concatenate((self.memories[0], arg1[np.newaxis, :, :, :]), axis = 0)
+            self.memories[1] = np.append(self.memories[1], arg2)
+            self.memories[2] = np.append(self.memories[2], arg2)
+            self.memories[3] = np.concatenate((self.memories[3], arg4[np.newaxis, :, :, :]), axis = 0)
 
-        if self.memories[0].shape[0] > self.capacity:
-            self.memories[0] = self.memories[0][1:]
-            self.memories[1] = self.memories[1][1:]
-            self.memories[2] = self.memories[2][1:]
-            self.memories[3] = self.memories[3][1:]
+        #if self.memories[0].shape[0] > self.write_capacity:
+            #self.memories[0] = self.memories[0][1:]
+            #self.memories[1] = self.memories[1][1:]
+            #self.memories[2] = self.memories[2][1:]
+            #self.memories[3] = self.memories[3][1:]
+        if self.memories[0].shape[0] == self.write_capacity and len(self.ret) == 0:
+            self.ret = self.memories
+            
+        #return
+
+        if self.memories[0].shape[0] >= self.write_capacity:
+            with h5py.File("replay_buffer/" + self.name + ".hdf5", 'a') as f:
+                obs_t_goal = f['obs_t_goal']
+                goal_or_action = f['goal_or_action']
+                reward = f['reward']
+                obs_tp1_goal = f['obs_tp1_goal']
+
+                if obs_t_goal.shape[0] >= self.capacity:
+                    obs_t_goal[:-self.write_capacity] = obs_t_goal[self.write_capacity:]
+                    obs_t_goal[-self.write_capacity:] = self.memories[0]
+
+                    goal_or_action[:-self.write_capacity] = goal_or_action[self.write_capacity:]
+                    goal_or_action[-self.write_capacity:] = self.memories[1]
+
+                    reward[:-self.write_capacity] = reward[self.write_capacity:]
+                    reward[-self.write_capacity:] = self.memories[2]
+
+                    obs_tp1_goal[:-self.write_capacity] = obs_tp1_goal[self.write_capacity:]
+                    obs_tp1_goal[-self.write_capacity:] = self.memories[3]
+
+                else:
+                    obs_t_goal.resize(tuple([obs_t_goal.shape[0] + self.write_capacity] + self.obs_goal_shape[1:]))
+                    obs_t_goal[-self.write_capacity:] = self.memories[0]
+
+                    goal_or_action.resize((goal_or_action.shape[0] + self.write_capacity, ))
+                    goal_or_action[-self.write_capacity:] = self.memories[1]
+
+                    reward.resize((reward.shape[0] + self.write_capacity, ))
+                    reward[-self.write_capacity:] = self.memories[2]
+                
+                    obs_tp1_goal.resize(tuple([obs_tp1_goal.shape[0] + self.write_capacity] + self.obs_goal_shape[1:]))
+                    obs_tp1_goal[-self.write_capacity:] = self.memories[3]
+                print("Memories stored: {0}".format(obs_tp1_goal.shape[0]))
+
+            self.memories = []
 
     def sample(self, size):
-        if size >= self.memories[0].shape[0]:
-            return self.memories
+        # TEST
         batch_idx = np.random.choice(self.memories[0].shape[0], size = size, replace = False)
-        memory_sample = [self.memories[0][batch_idx], self.memories[1][batch_idx],
-            self.memories[2][batch_idx], self.memories[3][batch_idx]]
+        return [self.memories[0][batch_idx], self.memories[1][batch_idx], self.memories[2][batch_idx], self.memories[3][batch_idx]]
+
+        with h5py.File("replay_buffer/" + self.name + ".hdf5", 'r') as f:
+            obs_t_goal = f['obs_t_goal']
+            goal_or_action = f['goal_or_action']
+            reward = f['reward']
+            obs_tp1_goal = f['obs_tp1_goal']
+
+            if obs_t_goal.shape[0] == 0:
+                memory_sample = self.memories
+            else:
+
+                if size >= obs_t_goal.shape[0]:
+                    memory_sample = [obs_t_goal[:], goal_or_action[:], reward[:], obs_tp1_goal[:]]
+                else:
+                    batch_idx = sorted(np.random.choice(obs_t_goal.shape[0], size = size, replace = False))
+                    memory_sample = [obs_t_goal[batch_idx], goal_or_action[batch_idx], reward[batch_idx], obs_tp1_goal[batch_idx]]
+
         return memory_sample
         
